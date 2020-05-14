a635bfa3c44dd569ba36d31777cc57ef2bdbcbb9
eriosstarks28 <58051709+eriosstarks28@users.noreply.github.com>
2020-05-14 14:28:19 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static('public'));
+app.use('/images', express.static('images'));
+app.set('view engine', 'pug');
+
+const routes = require('./routes/index');
+const projectRoutes = require('./routes/projects');
+app.use(routes, projectRoutes);
+
+
+
+app.use((err, req, res, next) => {
+    err = new Error('Page Not Found)');
+    err.status = 404;
+    next(err);
+});
+
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(404); 
+    res.render('error');
+});
+
+
+app.listen(3000, () => {
+    console.log('app is working');
+});
